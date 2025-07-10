1ab9d3608906baee6c8f3480f3d18d0b777ba2fe
Mercy0327 <mercy.phiri.mp@gmail.com>
2025-07-10 21:39:33 +0200

    week9
    functions and modules and Exception handling

---
+++ exception_handling.py
@@ -0,0 +1,19 @@
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+# except:
+#     print("Something went wrong")
+# finally:
+#     print("The 'try except' is finished")
+    
+else:
+    print("Everything went wrong")  
+    
+
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
+    
